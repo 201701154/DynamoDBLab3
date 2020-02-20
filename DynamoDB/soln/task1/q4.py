4c330da740e76f0d5200d70f34b15dadb0567946
KaranJhaveri <35135445+201701154@users.noreply.github.com>
2020-02-20 11:29:44 +0530

    Add files via upload

---
+++ DynamoDB/soln/task1/q4.py
@@ -0,0 +1,16 @@
+import boto3
+dynamodb = boto3.resource('dynamodb', region_name='ap-south-1', endpoint_url="http://localhost:8000")
+
+# Write Code here
+def getItem(table_name, key):
+  table = dynamodb.Table(table_name)
+  response = table.get_item(Key=key)
+  return response["Item"]
+
+if __name__ == "__main__":
+  dynamodb = boto3.resource('dynamodb', region_name='ap-south-1', endpoint_url="http://localhost:8000")#Scripts for operations on DynamoDB
+  obj = {
+    "title": "Learning JavaScript Design Patterns",
+    "publisher": "O'Reilly Media"
+  }
+  print(getItem("Books", obj))
---
+++ DynamoDB/soln/task1/q7.py
@@ -0,0 +1,18 @@
+import boto3
+from boto3.dynamodb.conditions import And, Attr
+import json
+
+
+if __name__ == "__main__":
+  dynamodb = boto3.resource('dynamodb', region_name='ap-south-1', endpoint_url="http://localhost:8000")
+
+  keyVal = {
+    "title": "Git Pocket Guide",
+    "publisher": "O'Reilly Media"
+  }
+
+  ##updatedVal = ["pages", "268"]
+  ##updateItem("Books", keyVal, updatedVal)
+  obj = dynamodb.Table("Books").scan(
+	FilterExpression=Attr("pages").gt(300))
+  print(obj["Items"])
